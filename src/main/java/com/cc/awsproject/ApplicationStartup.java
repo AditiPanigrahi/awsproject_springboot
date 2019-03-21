be264fc1027d24836004f379b8abd30c2fa6d829
piyushwaykole <piyushwaykole@gmail.com>
2019-03-20 17:57:13 -0700

    adding @Component annotation

--- src/main/java/com/cc/awsproject/ApplicationStartup.java
+++ src/main/java/com/cc/awsproject/ApplicationStartup.java
@@ -3,7 +3,9 @@ package com.cc.awsproject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent> {
 
 	@Value("${maxThreads}")
